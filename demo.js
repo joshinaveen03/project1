1dd75b719515c8313d4d5c49638953032f698e5e
joshinaveen03 <joshi.naveeen03@gmail.com>
2023-01-21 18:21:54 +0530

    project1

---
+++ demo.js
@@ -0,0 +1,17 @@
+//console.dir(document);
+var itemList= document.querySelector('#items');
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor='#f4f4f4';
+//console.log(itemList.parentNode.parentNode);
+//console.log(itemList.childNodes);
+
+//console.log(itemList.children);
+
+
+//console.log(itemList.children[1]);
+//itemList.children[1].style.backgroundColor='yellow'
+
+//console.log(itemList.nextSibling);
+var newDiv=document.createElement('div');
+newDiv.className='Hello';
+console.log(newDiv);
